8fb9d8f2b83488aeb5044455c10875d43e2f44e2
김 남규 <skarb629@gmail.com>
2018-11-21 18:21:04 +0900

    모듈화 버튼 라벨 패널

---
+++ mojavePos/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mojavePos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _Create ct = new _Create();
+
+            lbSet lb1 = new lbSet(this, "label1", "라벨이다asdfdsafa \n라벨.", 500, 300, 100, 100,10);
+            Label 라벨1 = ct.lable(lb1);
+            pnSet pn1 = new pnSet(this, 300, 300, 30, 30 );
+            Panel 패널 = ct.panel(pn1);
+            패널.BackColor = Color.Aqua;
+            btnSet btn1 = new btnSet(this, "btn1", "버튼", 50, 50, 100, 70, btn_Click);
+            Button button = ct.btn(btn1);
+
+            Controls.Add(button);
+            패널.Controls.Add(라벨1);
+            Controls.Add(패널);
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ mojavePos/_Create.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mojavePos
+{
+    class _Create
+    {
+        public Button btn(btnSet bS)
+        {
+            Button btn = new Button();
+            btn.DialogResult = DialogResult.OK;
+            btn.Name = btn.Name;
+            btn.Text = btn.Text;
+            btn.Size = new Size(bS.SX,bS.SY);
+            btn.Location = new Point(bS.PX,bS.PY);
+            btn.Cursor = Cursors.Hand;
+            btn.Click += bS.eh_btn;
+            return btn;
+        }
+
+        public Label lable(lbSet lb)
+        {
+            Label label = new Label();
+            label.Name = lb.Name;
+            label.Text = lb.Text;
+            label.Font = new Font("굴림",lb.FS);
+            label.Size = new Size(lb.SX, lb.SY);
+            label.Location = new Point(lb.PX, lb.PY);
+            label.BackColor = Color.Transparent;
+            return label;
+        }
+
+        public Panel panel(pnSet pn)
+        {
+            Panel panel = new Panel();
+            panel.Size = new Size(pn.SX,pn.SY);
+            panel.Location = new Point(pn.PX,pn.PY);
+            return panel;
+        }
+
+       
+    }
+}
