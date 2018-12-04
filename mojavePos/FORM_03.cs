4aae5a9f8162d747389491c064d7f956ad14c3ea
호성 천 <hosung1000@naver.com>
2018-12-04 18:42:31 +0900

    FORM_03 Panel만 만듬

--- mojavePos/FORM_03.cs
+++ mojavePos/FORM_03.cs
@@ -27,6 +27,28 @@ namespace mojavePos
             ClientSize = new Size(sX, sY);
             _Create ct = new _Create();
             Point_Print();
+
+
+            pnSet pn1 = new pnSet(this, 500, 750, 0, 0);
+            Panel 패널1 = ct.panel(pn1);
+            패널1.BackColor = Color.LightBlue;
+            Controls.Add(패널1);   //패널 화면 출력
+          
+
+            pnSet pn2 = new pnSet(this, 400, 350, 570, 200);
+            Panel 패널2 = ct.panel(pn2);
+            패널2.BackColor = Color.LightGray;
+            Controls.Add(패널2);   //패널 화면 출력
+
+            pnSet pn3 = new pnSet(this, 400, 350, 1040, 200);
+            Panel 패널3 = ct.panel(pn3);
+            패널3.BackColor = Color.LightGray;
+            Controls.Add(패널3);   //패널 화면 출력
+
+            pnSet pn4 = new pnSet(this, 1500, 150, 0, 800);
+            Panel 패널4 = ct.panel(pn4);
+            패널4.BackColor = Color.LightGray;
+            Controls.Add(패널4);   
         }
 
         ///////////////////////// 좌표 체크시 추가 /////////////////////////////
