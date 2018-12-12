a949e3285ffad78644ad8f5493be9915a5b74dd3
호성 천 <hosung1000@naver.com>
2018-12-12 18:21:50 +0900

    시계 날짜 연동 및 Menu->manager폼으로 이동.

--- mojavePos/Han/ManagerForm.cs
+++ mojavePos/Han/ManagerForm.cs
@@ -15,7 +15,8 @@ namespace mojavePos.Han
     {
         _Create ct = new _Create();
         Panel bottom;
-
+        private Graphics gr;
+        MenuForm menu;
 
         public ManagerForm()
         {
@@ -29,18 +30,68 @@ namespace mojavePos.Han
             this.IsMdiContainer = true;
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             Head();
+
+          
         }
+       
+       
         private void Head()
         {
             pnSet pn1 = new pnSet(this, 1500, 100, 0, 0);
             Panel head = ct.panel(pn1);
             Controls.Add(head);
-            pnSet pn2 = new pnSet(this, 1500, 800, 0, 100);
-            bottom = ct.panel(pn2);
-            Controls.Add(bottom);
-            head.BackColor = Color.Black;
-            bottom.BackColor = Color.Brown;
+            /*-------------메뉴박스------------------*/
 
+            lbSet lb1 = new lbSet(this, "lb1", "Category", 200, 35, 350, 80, 25);
+            Label 카테고리 = ct.lable(lb1);
+            카테고리.TextAlign = ContentAlignment.MiddleCenter;
+            Controls.Add(카테고리);
+
+            btnSet btn_1 = new btnSet(this, "btn_1", "//사진으로 대체", 30, 30, 460, 125, btn_Click);
+            Button 카테고리추가버튼 = ct.btn(btn_1);
+            Controls.Add(카테고리추가버튼);
+
+            pictureBoxSet picturebox_1 = new pictureBoxSet(this, 40, 40, 410, 120, " ");
+            PictureBox 카테고리픽쳐 = ct.picture(picturebox_1);
+            카테고리픽쳐.BackColor = Color.Green;
+            Controls.Add(카테고리픽쳐);
+
+            lbSet lb2 = new lbSet(this, "lb2", "Menu", 200, 35, 1000, 80, 25);
+            Label 메뉴 = ct.lable(lb2);
+            카테고리.TextAlign = ContentAlignment.MiddleCenter;
+            Controls.Add(메뉴);
+
+            btnSet btn_2 = new btnSet(this, "btn_2", "//사진으로 대체", 30, 30, 1060, 125, btn2_Click);
+            Button 메뉴추가버튼 = ct.btn(btn_2);
+            Controls.Add(메뉴추가버튼);
+
+            pictureBoxSet picturebox_2 = new pictureBoxSet(this, 40, 40, 1010, 120, " ");
+            PictureBox 메뉴픽쳐 = ct.picture(picturebox_2);
+            메뉴픽쳐.BackColor = Color.Green;
+            Controls.Add(메뉴픽쳐);
+
+            /*-----------*/
+            gr = this.CreateGraphics();
+            Pen pen1 = new Pen(Color.Black, 1);
+            pen1.LineJoin = System.Drawing.Drawing2D.LineJoin.Bevel;
+            Point First = new Point(300, 300);
+            Point Second = new Point(1200, 300);
+            /*-----------*/
+
+            pnSet pn2 = new pnSet(this, 1500, 800, 0, 100);
+            bottom = ct.panel(pn2); // 패널이름 : bottom
+            Controls.Add(bottom);
+            bottom.Controls.Add(카테고리);
+            bottom.Controls.Add(카테고리추가버튼);
+            bottom.Controls.Add(카테고리픽쳐);
+            bottom.Controls.Add(메뉴);
+            bottom.Controls.Add(메뉴추가버튼);
+            bottom.Controls.Add(메뉴픽쳐);
+            //bottom.Controls.Add(pen1);
+
+            head.BackColor = Color.Black;
+            bottom.BackColor = Color.BurlyWood;
+            /*----------------------------*/
             ArrayList btn_list = new ArrayList();
             btn_list.Add(new btnSet(this, "menu", "메뉴관리", 375, 100, 0, 0, Main_Click));
             btn_list.Add(new btnSet(this, "money", "매출관리", 375, 100, 375, 0, Main_Click));
@@ -89,6 +140,13 @@ namespace mojavePos.Han
                     break;
             }
         }
-  
+        private void btn_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
