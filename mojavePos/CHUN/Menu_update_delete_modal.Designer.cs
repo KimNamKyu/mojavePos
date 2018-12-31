9fc10388a9a7a77bca17fc733e7841de9b98260d
Han <lhu3319@nate.com>
2018-12-31 14:03:41 +0900

    a

--- mojavePos/CHUN/Menu_update_delete_modal.Designer.cs
+++ mojavePos/CHUN/Menu_update_delete_modal.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+           // this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Menu_update_delete_modal";
             this.Text = "Menu_uodate_delete_modal";
             this.ResumeLayout(false);
